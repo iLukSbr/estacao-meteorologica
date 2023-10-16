311ebc10a958534eb03f765f0d3507fd6146504f
Jocarele <145937763+Jocarele@users.noreply.github.com>
2023-10-16 15:01:39 -0300

    Update ITGMPU6050.cpp
    Verificar corretude

--- src/ITGMPU6050.cpp
+++ src/ITGMPU6050.cpp
@@ -51,16 +51,75 @@ void ITGMPU6050::print() const{
 }
 
 void ITGMPU6050::read(){
-    mpu6050->update();
-    info[0] = mpu6050->getAngleX();
-    info[1] = mpu6050->getAngleY();
-    info[2] = mpu6050->getAngleZ();
+    gyro_signals();
+    info[0] = info[0] - cali[0];
+    info[1] = info[1] - cali[1];
+    info[2] = info[2] - cali[2];
     // info[3] = mpu6050->getTemp();
 }
 
 void ITGMPU6050::start(){
     Wire.begin();
-    mpu6050->begin();
-    mpu6050->calcGyroOffsets(true);
+    
+    Wire.setClock(400000); //especificação do sensor
+    Wire.begin();
+    delay(250);
+    //Coloca o gyroscopio no modo energia
+    Wire.beginTransmission(0x68);
+    Wire.write(0x6B);
+    Wire.write(0x00);
+    Wire.endTransmission();
+    Serial.println("\nCALIBRANDO");
+    
+    float RateCaliRoll,RateCaliPitch,RateCaliYaw;
+    int RateCaliNumber;
+    int maximo = 2000;
+    for(RateCaliNumber=0;RateCaliNumber<maximo;RateCaliNumber++)
+    {
+        gyro_signals();
+        RateCaliRoll+=info[0];
+        RateCaliPitch+=info[1];
+        RateCaliYaw+=info[2];
+        delay(1);
+    }
+    RateCaliRoll/=maximo;
+    RateCaliPitch/=maximo;
+    RateCaliYaw/=maximo;
+    Serial.println("Calibrado");
+    cali[0] = RateCaliRoll;
+    cali[1] = RateCaliPitch;
+    cali[1] = RateCaliYaw;
     started = true;
 }
+
+void gyro_signals()
+{
+      //Filtro passa baixa
+  // Verificar se realmente é necessario, o exemplo abaixo é um código de um drone
+  Wire.beginTransmission(0x68);// Começa comunicação I2C com gyroscopio
+  Wire.write(0x1A);            // 0X1A = Endereço do registrador
+  Wire.write(0x05);            // frequecia de corte de 10 HZ
+  Wire.endTransmission();
+
+  //Setar o fator de sensitividade da escala
+  Wire.beginTransmission(0x68);// Começa comunicação I2C com gyroscopio
+  Wire.write(0x1B);            // 1B = registrador para alterar a escala
+  Wire.write(0x8);             // 65.5 LSB/°/S
+  Wire.endTransmission();
+
+  //Acesso das medições do gyroscopio
+  Wire.beginTransmission(0x68);// Começa comunicação I2C com gyroscopio           
+  Wire.write(0x43);            //Primeiro registrador
+  Wire.endTransmission();
+
+  Wire.requestFrom(0x68,6);  
+
+  int16_t GyroX=Wire.read()<<8 | Wire.read();  //Read the gyro measures around the X axis
+  int16_t GyroY=Wire.read()<<8 | Wire.read();
+  int16_t GyroZ=Wire.read()<<8 | Wire.read();
+
+  info[0]=(double)GyroX/65.5;
+  info[1]=(double)GyroY/65.5;
+  info[2]=(double)GyroZ/65.5;
+}
+    
