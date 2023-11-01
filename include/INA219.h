c9078cd8d71becf04b91b57d457d48793dff430b
iLukSbr <lucasyfm@hotmail.com>
2023-11-01 15:04:06 +0000

    INA219 start prototype

--- include/INA219.h
+++ include/INA219.h
@@ -1,4 +1,33 @@
+/* Voltage and current sensor INA219 */
+
+#pragma once
+
+#include <INA219_WE.h>
+
+#include "Component.h"
+
 #ifndef _INA219
     #define _INA219
     #define _INA219_ 1
 #endif
+
+#define SHUNT_RESISTANCE 100
+
+class INA219 : public Component{
+    private:
+        float info[3];
+
+        INA219_WE* multi;
+
+    public:
+        INA219();
+        ~INA219();
+
+        float getCurrent();
+        float getPercentage();
+        float getVoltage();
+
+        void print() const override;
+        void read() override;
+        void start() override;
+};
---
+++ src/INA219.cpp
@@ -0,0 +1,53 @@
+/* Voltage and current sensor INA219 */
+
+#include <INA219.h>
+
+INA219::INA219():
+    multi(new INA219_WE(INA219_I2C_ADDRESS))
+{
+    start();
+}
+
+INA219::INA219(){
+    
+}
+
+float INA219::getCurrent(){
+    return info[0];
+}
+
+float INA219::getPercentage(){
+    rerurn info[1];
+}
+
+float INA219::getVoltage(){
+    return info[2];
+}
+
+void INA219::print() const{
+    Serial.print(F("INA219 "));
+    Serial.print(name);
+    Serial.println(F(:));
+    Serial.print(F("current = "));
+    Serial.println(info[0]);
+    Serial.print(F("percentage = "));
+    Serial.println(info[1]);
+    Serial.print(F("voltage = "));
+    Serial.println(info[2]);
+}
+
+void INA219::read(){
+    
+}
+
+void INA219::start(){
+    if(multi->init()){
+        started = true;
+        multi->setADCMode(SAMPLE_MODE_128)
+        multi->setMeasureMode(CONTINUOUS);
+        multi->setPGain(amp_def);
+        multi->setBusRange(volt_def);
+        multi->setCorrectionFactor(calibration_factor);
+        multi->setShuntVoltOffset(calibration_volt);
+    }
+}
