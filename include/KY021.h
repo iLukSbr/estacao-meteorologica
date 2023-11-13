4250a5b3a889a51e33c54bb9f1f6d71c6c069db2
iLukSbr <lucasyfm@hotmail.com>
2023-11-12 22:40:43 -0300

    rain correction

--- include/KY021.h
+++ include/KY021.h
@@ -1,34 +1,34 @@
-// /* KY-021 reed switch module */
+/* KY-021 reed switch module for rain sensor tipping bucket */
 
-// #pragma once
+#pragma once
 
-// #ifndef _KY021
-//     #define _KY021
-//     #define _KY021_ 1
-// #endif
+#ifndef _KY021
+    #define _KY021
+    #define _KY021_ 1
+#endif
 
-// #include "Component.h"
-// #include "MHRD.h"
+#include "Component.h"
+#include "MHRD.h"
 
-// #define KY021_PIN 3
-// #define SEESAW_VOLUME 0.01// liter
-// #define FUNNEL_AREA 0.5// m^2
+#define KY021_PIN 3
+#define SEESAW_VOLUME 0.01// liter
+#define FUNNEL_AREA 0.5// m^2
 
-// class KY021 : public Component{
-//     private:
-//         bool seesaw_state;
+class KY021 : public Component{
+    private:
+        bool seesaw_state;
 
-//         float info;
+        float info;
 
-//         long last_tip;
+        long last_tip;
 
-//         MHRD* rainmeter;
+        MHRD* rainmeter;
 
-//     public:
-//         KY021();
-//         ~KY021();
-//         float getRainfall() const;
-//         void print() const override;
-//         void read() override;
-//         void start() override;
-// };
+    public:
+        KY021();
+        ~KY021();
+        float getRainfall() const;
+        void print() const override;
+        void read() override;
+        void start() override;
+};
